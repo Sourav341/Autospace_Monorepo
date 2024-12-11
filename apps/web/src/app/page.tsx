fd5cae53ebfad6047e3218567d8fd501cd233ecb
Sourav341 <rajsourav.singh58@gmail.com>
2024-12-11 17:24:31 +0530

    Changes made

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,5 @@
-import {add} from "@autospace/sample-lib";
+import { add } from '@autospace/sample-lib'
 
 export default function Home() {
-  return (
-    <main> Hello World ! {add (34, 78)}</main>
-  );
+  return <main> Hello World ! {add(34, 78)}</main>
 }
